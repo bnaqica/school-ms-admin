e352dabf9bb57ef92cc95033dd3f4d058eeb1916
Brian Naqica <bnaqica@yahoo.com>
2019-11-26 22:08:41 -0700

    Working SpringBoot Admin tool

--- src/main/java/com/bnaqica/schoolmsadmin/SchoolMsAdminApplication.java
+++ src/main/java/com/bnaqica/schoolmsadmin/SchoolMsAdminApplication.java
@@ -1,13 +1,17 @@
 package com.bnaqica.schoolmsadmin;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableAdminServer
 public class SchoolMsAdminApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SchoolMsAdminApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolMsAdminApplication.class, args);
+    }
 
 }
