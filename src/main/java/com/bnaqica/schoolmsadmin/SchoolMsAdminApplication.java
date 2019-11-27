56ff7e6a2d205cfa3f6667954e31dd11ee505fa3
Brian Naqica <bnaqica@yahoo.com>
2019-11-26 20:07:33 -0700

    Initial skeleton commit

---
+++ src/main/java/com/bnaqica/schoolmsadmin/SchoolMsAdminApplication.java
@@ -0,0 +1,13 @@
+package com.bnaqica.schoolmsadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolMsAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolMsAdminApplication.class, args);
+	}
+
+}
